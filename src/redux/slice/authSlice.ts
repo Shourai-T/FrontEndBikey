6e1d57d907b3d371d45bfeacc4eb6284ba8ef87e
Mtri nèeeeeee <90810549+mtrine@users.noreply.github.com>
2025-03-21 18:56:04 +0700

    send otp

--- src/redux/slice/authSlice.ts
+++ src/redux/slice/authSlice.ts
@@ -1,20 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { sendOTP } from "../api_request/auth_api";
 
 const authSlice = createSlice({
     name: 'auth',
-    initialState:{
-        login:{
+    initialState: {
+        login: {
             currentUser: null,
             isFetching: false,
             error: false
         },
-        register:{
+        register: {
             currentUser: null,
             isFetching: false,
             error: false
         },
-        logout:{
-            currentUser: null,
+        logout: {
+            isFetching: false,
+            error: false
+        },
+        verifyOtp: {
+            success: true,
+            isFetching: false,
+            error: false
+        },
+        sendOTP: {
+            success: true,
             isFetching: false,
             error: false
         }
@@ -24,21 +34,21 @@ const authSlice = createSlice({
             state.login.isFetching = true;
             state.login.error = false;
         },
-        loginSuccess:(state,actions)=>{
+        loginSuccess: (state, actions) => {
             state.login.currentUser = actions.payload;
             state.login.isFetching = false;
             state.login.error = false;
         },
-        loginFailure:(state) => {
-            state.login.error= true;
+        loginFailure: (state) => {
+            state.login.error = true;
             state.login.isFetching = false;
         },
         registerStart(state) {
             state.register.error = false;
             state.register.isFetching = false;
         },
-        registerSuccess(state,actions) {
-            state.register.currentUser = actions.payload;
+        registerSuccess(state, actions) {
+            state.login.currentUser = actions.payload;
             state.register.error = false;
             state.register.isFetching = false;
         },
@@ -50,14 +60,42 @@ const authSlice = createSlice({
             state.logout.isFetching = true;
             state.logout.error = false;
         },
-        logoutSuccess(state){
-            state.logout.currentUser = null;
+        logoutSuccess(state) {
+            state.login.currentUser = null
             state.logout.isFetching = false;
             state.logout.error = false;
         },
         logoutFailure(state) {
             state.logout.error = true;
             state.logout.isFetching = false;
+        },
+        verifyOtpStart(state) {
+            state.verifyOtp.isFetching = true;
+            state.verifyOtp.error = false;
+        },
+        verifyOtpSuccess(state) {
+            state.verifyOtp.success = true;
+            state.verifyOtp.isFetching = false;
+            state.verifyOtp.error = false;
+        },
+        verifyOtpFailure(state) {
+            state.verifyOtp.success = false;
+            state.verifyOtp.isFetching = false;
+            state.verifyOtp.error = true;
+        },
+        sendOTPStart(state) {
+            state.sendOTP.isFetching = true;
+            state.sendOTP.error = false;
+        },
+        sendOTPSuccess(state) {
+            state.sendOTP.success = true;
+            state.sendOTP.isFetching = false;
+            state.sendOTP.error = false;
+        },
+        sendOTPFailure(state) {
+            state.verifyOtp.success = false;
+            state.verifyOtp.isFetching = false;
+            state.verifyOtp.error = true;
         }
     }
 })
@@ -67,5 +105,9 @@ export const { registerStart, registerSuccess, registerFailure } = authSlice.act
 export const { loginStart, loginSuccess, loginFailure } = authSlice.actions;
 
 export const { logoutStart, logoutSuccess, logoutFailure } = authSlice.actions;
+
+export const { verifyOtpStart, verifyOtpSuccess, verifyOtpFailure } = authSlice.actions;
+
+export const { sendOTPStart, sendOTPSuccess, sendOTPFailure } = authSlice.actions;
 
 export default authSlice.reducer;
